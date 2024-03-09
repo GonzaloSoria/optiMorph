f743c0182c78aa126cc19d0c8d9b8980da27d697
GonzaloSoria <gonzalo.sooria@gmail.com>
2024-03-08 21:31:56 -0300

    add formik to manage the form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,48 +1,56 @@
 import { useState } from 'react';
+import { Field, Form, Formik } from 'formik';
 import './form.css';
 
-export const Form = () => {
+export const UrlForm = () => {
     const [url, setUrl] = useState('');
     const [result, setResult] = useState('');
 
-    const handleForm = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await fetch('https://opti-morph-back.vercel.app/process-url', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ url }),
-            });
+    const handleForm = async (values) => {
+        
+        console.log(values.url);
+        // try {
+        //     const response = await fetch('https://opti-morph-back.vercel.app/process-url', {
+        //       method: 'POST',
+        //       headers: {
+        //         'Content-Type': 'application/json',
+        //       },
+        //       body: JSON.stringify(values.url),
+        //     });
       
-            if (response.ok) {
-              const data = await response.json();
-              setResult(data.message);
-            } else {
-              setResult('Error al procesar la URL');
-            }
-          } catch (error) {
-            console.error('Error en la solicitud:', error.message);
-          }
+        //     if (response.ok) {
+        //       const data = await response.json();
+        //       setResult(data.message);
+        //     } else {
+        //       setResult('Error al procesar la URL');
+        //     }
+        //   } catch (error) {
+        //     console.error('Error en la solicitud:', error.message);
+        //   }
     }
 
     return (
         <>
-            <form id="form-container w-auto" onSubmit={handleForm}>
-                <input 
-                    type="text" 
-                    name="url" 
-                    placeholder="ejemplo: https://wwww.lenovo.com/us/en/pc"
-                    id="url" 
-                    className='form-input px-8 py-2'
+          <Formik
+            initialValues={{url: ''}}
+            onSubmit={handleForm}
+          >
+            {({handleChange}) => (
+              <Form className='form-container sw-auto'>
+                <Field 
+                  id="url"
+                  name="url" 
+                  type="text"
+                  className="form-input px-8 py-2"
+                  onChange={handleChange}
                 />
                 <button 
-                    id="url"
                     className='text-black bg-white px-8 py-2 ml-5 rounded-md'
-                >Buscar</button>
-            </form>
+                    type='submit'
+                >Escanear</button>
+              </Form>
+            )}
+          </Formik>
         </>
     )
 }
